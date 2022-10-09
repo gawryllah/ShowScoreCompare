b3e203d57fe771c99386650abd231e0ff42c5713
gawryllah <gawrysjak@gmail.com>
2022-10-09 23:28:35 +0200

    fixed nullable object

--- ShowScoreCompare/Services/MovieDbService.cs
+++ ShowScoreCompare/Services/MovieDbService.cs
@@ -17,22 +17,44 @@ namespace ShowScoreCompare.Services
 
             var content = await response.Content.ReadAsStringAsync();
 
-            ShowDTO? movie = await Task.Run(() => JsonConvert.DeserializeObject<ShowDTO>(content));
+            var root = await Task.Run(() => JsonConvert.DeserializeObject<Rootobject>(content));
+
+            var movie = new ShowDTO()
+            {
+                overview = root.results[0].overview,
+                popularity = root.results[0].popularity,
+                poster_path = root.results[0].poster_path,
+                release_date = root.results[0].release_date,
+                title = root.results[0].title,
+                vote_average = root.results[0].vote_average,
+                vote_count = root.results[0].vote_count
+            };
+
 
             return movie;
-
-
         }
 
         public async Task<ShowDTO> GetSeries(string title, string key)
         {
-            var response = await _httpClient.GetAsync($"search/tv?api_key={key}&query={title}");
+            var response = await _httpClient.GetAsync($"search/movie?api_key={key}&query={title}");
 
             var content = await response.Content.ReadAsStringAsync();
 
-            var series = await Task.Run(() => JsonConvert.DeserializeObject<List<ShowDTO>>(content));
+            var root = await Task.Run(() => JsonConvert.DeserializeObject<Rootobject>(content));
 
-            return series[0];
+            var series = new ShowDTO()
+            {
+                overview = root.results[0].overview,
+                popularity = root.results[0].popularity,
+                poster_path = root.results[0].poster_path,
+                release_date = root.results[0].release_date,
+                title = root.results[0].title,
+                vote_average = root.results[0].vote_average,
+                vote_count = root.results[0].vote_count
+            };
+
+
+            return series;
         }
     }
 }
